519b4ea78bd5917a2f8c9c8fef7976d4aca6f312
krumiprog <krumiprog@gmail.com>
2021-05-14 19:33:54 +0300

    feat: users processing

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,38 @@
-const getAll = async () => 
-  // TODO: mock implementation. should be replaced during task development
-   []
-;
+const DB = require('../../db/inMemoryDb');
 
-module.exports = { getAll };
+const getAll = async () => DB.users;
+
+const getById = async (id) => {
+  const match = DB.users.find((user) => user.id === id);
+  if (!match) {
+    // TODO: not found
+  }
+  return match;
+};
+
+const save = async (user) => {
+  DB.users.push(user);
+  return user;
+};
+
+const update = async (id, newUser) => {
+  const match = DB.users.find((user) => user.id === id);
+  if (!match) {
+    // TODO: not found
+  }
+  match.name = newUser.name;
+  match.login = newUser.login;
+  match.password = newUser.password;
+  return match;
+};
+
+const remove = async (id) => {
+  const match = DB.users.findIndex((user) => user.id === id);
+  if (match !== -1) {
+    // TODO: not found
+  }
+  DB.users.splice(match, 1);
+  // DB.users = DB.users.filter((user) => user.id !== id);
+};
+
+module.exports = { getAll, getById, save, update, remove };
