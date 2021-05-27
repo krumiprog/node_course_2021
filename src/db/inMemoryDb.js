cc3b4ff68f62d895bccd5b0a9abc0350efe29d2e
krumiprog <krumiprog@gmail.com>
2021-05-27 08:22:58 +0300

    doc: jsdoc, describe database

--- src/db/inMemoryDb.js
+++ src/db/inMemoryDb.js
@@ -1,3 +1,14 @@
+/**
+ * Database.
+ * @typedef {Object} Database
+ * @property {User[]} users - An array of users.
+ * @property {Board[]} boards - An array of boards.
+ * @property {Task[]} tasks - An array of tasks.
+ */
+
+/**
+ * @type {Database}
+ */
 const db = {
   users: [],
   boards: [],
