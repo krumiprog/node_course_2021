13ac61ace66a3d7ecf043e99b7c5b96eab5ddc89
krumiprog <krumiprog@gmail.com>
2021-07-05 19:23:57 +0300

    refactor: delete express app

--- express-app/src/utils/toResponse.ts
+++
@@ -1,6 +0,0 @@
-import { User } from '../resources/entities/user';
-
-export const toResponse = (user: User): Omit<User, 'password' | 'tasks'> => {
-  const { id, name, login } = user;
-  return { id, name, login };
-};
