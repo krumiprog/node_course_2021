721adcd6a0f4662f51d87b2d9104edb8eccfdb20
krumiprog <krumiprog@gmail.com>
2021-07-05 16:35:31 +0300

    feat: add swagger

--- nest-app/src/main.ts
+++ nest-app/src/main.ts
@@ -1,9 +1,18 @@
+import * as path from 'path';
+import * as yaml from 'yamljs';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
 import { CONFIG } from './common/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerDocument = yaml.load(
+    path.join(__dirname, '..', 'doc', 'api.yaml'),
+  );
+  SwaggerModule.setup('doc', app, swaggerDocument);
+
   await app.listen(CONFIG.PORT);
 }
 bootstrap();
