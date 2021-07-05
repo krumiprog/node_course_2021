36189b697bbf77b0229e9d21d164b3c245e3164c
krumiprog <krumiprog@gmail.com>
2021-07-05 17:10:08 +0300

    feat: add fastify

--- nest-app/src/main.ts
+++ nest-app/src/main.ts
@@ -4,15 +4,28 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule } from '@nestjs/swagger';
 import { CONFIG } from './common/config';
+import { INestApplication } from '@nestjs/common';
+import { NestFastifyApplication } from '@nestjs/platform-fastify';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const { PORT, USE_FASTIFY } = CONFIG;
+
+  let app: INestApplication;
+
+  if (USE_FASTIFY === 'true') {
+    app = await NestFactory.create<NestFastifyApplication>(AppModule);
+  } else {
+    app = await NestFactory.create<NestExpressApplication>(AppModule);
+  }
 
   const swaggerDocument = yaml.load(
     path.join(__dirname, '..', 'doc', 'api.yaml'),
   );
   SwaggerModule.setup('doc', app, swaggerDocument);
 
-  await app.listen(CONFIG.PORT);
+  await app.listen(PORT, () =>
+    console.log(`App is running on http://localhost:${PORT}`),
+  );
 }
 bootstrap();
